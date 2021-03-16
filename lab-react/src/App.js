b6d244562c293c69c411c31bda71279a18cf05c9
Isaac Mendez <IsaacMendez.work@gmail.com>
2021-03-16 10:17:33 -0400

    Added React file for lab

--- lab-react/src/App.js
+++ lab-react/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          This is a practice paragraph I'm trying in src/App.js and using HTML-style formatting. 
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
